20e0da852d69c155a12494c5f0ab9523c2655bbb
FrazierMark <m.mark.frazier@gmail.com>
2024-03-20 16:15:03 -0700

    sorting out sequelize and postgres, working now

--- app.js
+++ app.js
@@ -22,19 +22,59 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// BODY-PARSER
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Models
+const models = require('./db/models');
+
 // Mock Data
 let events = [
-  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
-]
+	{
+		title: 'I am your first event',
+		desc: 'A great event that is super fun to look at and good',
+		imgUrl:
+			'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+	},
+	{
+		title: 'I am your second event',
+		desc: 'A great event that is super fun to look at and good',
+		imgUrl:
+			'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+	},
+	{
+		title: 'I am your third event',
+		desc: 'A great event that is super fun to look at and good',
+		imgUrl:
+			'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+	},
+];
 
 // Routes
 
 // Index
 app.get('/', (req, res) => {
-  res.render('events-index', { events: events });
-})
+	models.Event.findAll({ order: [['createdAt', 'DESC']] }).then((events) => {
+		res.render('events-index', { events: events });
+	});
+});
+
+// New Event Route
+app.get('/events/new', (req, res) => {
+	res.render('events-new', {});
+});
+
+// Create Event Route
+app.post('/events', (req, res) => {
+	models.Event.create(req.body)
+		.then((event) => {
+			res.redirect(`/`);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
